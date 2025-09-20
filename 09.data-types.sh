31178cbbe920fe10509f29d6975926347aeb9cb2
Poguri Diya <diya>
2025-09-20 19:02:37 +0530

    some messages

--- 09.data-types.sh
+++ 09.data-types.sh
@@ -9,3 +9,10 @@ SUM=$(($NUMBER1+NUMBER2+$NAME))
 
 echo "SUM is: ${SUM}"
 
+# Size = 4, Max index = 3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}"
+
