6aceccea408f2623d365d225db1aa5e0e04c6753
Poguri Diya <diya>
2025-09-20 17:12:20 +0530

    some messages

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# No space before or after equal
+PERSON1=sai
+PERSON2=diya
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: i am learning shell scripting $PERSON1"
