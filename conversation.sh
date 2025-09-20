c8000fb8821a53895d58f581200d022e53bab5dc
Poguri Diya <diya>
2025-09-20 16:57:38 +0530

    some messages

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: what are you learning recently?"
+echo "suresh:: i am learning shell scripting Ramesh"
