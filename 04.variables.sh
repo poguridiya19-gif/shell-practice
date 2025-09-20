b194f7c5f952b2c4d96f77257283b3f5724e26da
Poguri Diya <diya>
2025-09-20 17:36:18 +0530

    some messages

--- 04.variables.sh
+++ 04.variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:: Welcome to amalapuram $PERSON1
+echo "$PERSON1:: Welcome to amalapuram $PERSON2
 echo "$PERSON2:: Thank yoou $PERSON1, FINALLY not on zoom webinar"
 echo "$PERSON1:: so ,do we have deal "
 echo "$PERSON2:: yes, if we join ukraine to russia"
