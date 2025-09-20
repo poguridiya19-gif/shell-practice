6f3d09e1f70140b549f7b22eb75ae2ce493c22b4
Poguri Diya <diya>
2025-09-20 18:46:20 +0530

    some messages

--- 08.special-vars.sh
+++ 08.special-vars.sh
@@ -2,4 +2,7 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
-
+echo"script name:$0"
+echo "current directory: $PWD"
+echo "who is running this: $USER"
+echo "Home directory of user: $HOME"
