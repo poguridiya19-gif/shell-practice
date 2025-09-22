c02d91757b8bc7e3d80662997ec836a6a4d3a976
Poguri Diya <diya>
2025-09-22 14:24:44 +0530

    session-o6

--- 11.conditions.sh
+++ 11.conditions.sh
@@ -4,7 +4,9 @@ echo " please enter the number "
 read NUMBER
 
 if [ $(($NUMBER % 2)) -eq 0 ]; then
-    echo " given number $NUMBER is even "
+    echo " given number $NUMBER is prime number "
 else
-    echo " given number $NUMBER is odd "
+    echo " given number $NUMBER is composite number "
 fi
+
+
