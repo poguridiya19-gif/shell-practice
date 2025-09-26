bc67b598b13cb732e77d8f1ef8e2a01561f2658d
Poguri Diya <diya>
2025-09-26 16:42:25 +0530

    session-6

--- practice.sh
+++ practice.sh
@@ -3,7 +3,7 @@
 echo "please enter the number"
 read number
 
-if [$(($number % 2)) -eq 0]; then
+if [$ (($number % 2)) -eq 0]; then
     echo "given $number is even"
 else
     echo "given $number is odd"
