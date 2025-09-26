f2e5536a2372e687722f1da2e222d74eef370eaa
Poguri Diya <diya>
2025-09-26 17:19:59 +0530

    session-6

--- practice.sh
+++ practice.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-userid=$(id -u)
-
-if [ $userid -ne 0 ]; then
-    echo "ERROR:: please run these script with root privelage"
-    exit 1 #failure other than zero
-fi
-
-dnf install mysql -y
-
-if [ $? -ne 0 ]; then
-    echo "ERROR:: installing mysql is failure"
-    exit 1
-else
-    echo "installing mysql is success"
-fi
-
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
