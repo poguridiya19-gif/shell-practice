a08945a7a7b5b3c890056e226ce6095790a89c79
Poguri Diya <diya>
2025-09-26 16:59:23 +0530

    session-6

--- practice.sh
+++ practice.sh
@@ -3,6 +3,6 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "error:: please run the script with root privilage"
+    echo "error:: please run the script with root privelege"
     exit 1
 fi
