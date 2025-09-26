faadee85d3684bf763a8f04393641e1f0f93fd0a
Poguri Diya <diya>
2025-09-26 17:19:19 +0530

    session-6

--- practice.sh
+++ practice.sh
@@ -15,3 +15,19 @@ if [ $? -ne 0 ]; then
 else
     echo "installing mysql is success"
 fi
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run these script with root privelege"
+    exit 1 # failure is other than zero
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Error:: Installing mysql is failure"
+    exit 1
+else
+    echo "Installing mysql is Success"
+fi
