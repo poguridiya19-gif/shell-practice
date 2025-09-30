b6f23dcf17665f9ae999d1de16c48433740746fc
Poguri Diya <diya>
2025-09-30 14:33:13 +0530

    session-06

--- 12.installation.sh
+++ 12.installation.sh
@@ -3,15 +3,33 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0 
+    echo "ERROR:: Please run these script with root privelege"
+    exit 1 # factor is other than 0 
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing MySQL is failure"
+    echo "ERROR:: Installing mysql is failure"
     exit 1
 else
-    echo "Installing MySQL is SUCCESS"
-fi 
+    echo "Installing mysql is success"
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing nginx is failure"
+    exit 1
+else
+    echo "Installing nginx is success"
+fi
+
+dnf install python3 -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing python3 is failure"
+    exit 1
+else
+    echo "Installing python3 is success"
+fi
