5ee15697c4d9ab5c9b1074d3508aa93860724b85
Poguri Diya <diya>
2025-09-30 14:29:53 +0530

    session-06

--- practice.sh
+++ practice.sh
@@ -15,3 +15,21 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing mysql is success"
 fi
+
+dnf installing nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing nginx is failure"
+    exit 1
+else
+    echo "Installing nginx is success"
+fi
+
+dnf installing python -3
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing python is failure"
+    exit 1
+else
+    echo "Installing python is success"
+fi
