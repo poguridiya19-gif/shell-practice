2fc428960e3179a2caa4604b64ae31773df0c71f
Poguri Diya <diya>
2025-09-30 14:15:37 +0530

    session-06

--- practice.sh
+++ practice.sh
@@ -2,11 +2,16 @@
 
 USERID=$(id -u)
 
-if ($USERID  -ne  0); then
-    echo "error:: please run with root privilage"
-    exit 1 #factor other than 0
+if [$USERID  -ne  0]; then
+    echo "error:: please run these script with root privelage"
+    exit 1 # factor is other than 0
 fi
 
-#gt
-#lt
-#ne
+dnf install mysql -y
+
+if [$? -ne 0]; then
+    echo "error:: installing mysql is failure"
+    exit 1
+else
+    echo "installing mysql is success"
+fi
