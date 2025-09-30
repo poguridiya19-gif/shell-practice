f73e083f616a472c1bb712e13f4cc88f87799fcb
Poguri Diya <diya>
2025-09-30 14:20:31 +0530

    session-06

--- practice.sh
+++ practice.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [$USERID  -ne  0]; then
     echo "error:: please run these script with root privelage"
-    exit 1 # factor is other than 0 success (command ran without errors).
+    exit 1 # factor is other than 0 
 fi
 
 dnf install mysql -y
