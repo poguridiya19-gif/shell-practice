e53bf1f5feec92c8324c102bc7f257e5befe2ba0
Poguri Diya <diya>
2026-05-19 17:10:22 +0530

    shell

--- exercise.sh
+++ exercise.sh
@@ -2,5 +2,10 @@
 # VARIABLE="shell scripting is fun!"
 # echo $VARIABLE
 
-HOSTNAME=diya
-echo  "this script is running on $HOSTNAME"
+# HOSTNAME=diya # no need $ for names like other also no need "" , () these r not neccesity
+# echo  "this script is running on $HOSTNAME"
+
+FILE_PATH="/home/ec2-user"
+if [-e "$FILE_PATH"]; then
+   echo "$FILE_PATH paswords are enabled"
+fi   
