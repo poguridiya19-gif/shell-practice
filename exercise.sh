a38ee80839502dbf32ed670f43b26632c4024c44
Poguri Diya <diya>
2026-05-19 17:22:44 +0530

    shell

--- exercise.sh
+++ exercise.sh
@@ -5,13 +5,19 @@
 # HOSTNAME=diya # no need $ for names like other also no need "" , () these r not neccesity
 # echo  "this script is running on $HOSTNAME"
 
-FILE_PATH="/home/ec2-user"
-if [ -e "$FILE_PATH" ]; then
-   echo "$FILE_PATH paswords are enabled"
-fi   
+# FILE_PATH="/home/ec2-user"
+# if [ -e "$FILE_PATH" ]; then
+#    echo "$FILE_PATH paswords are enabled"
+# fi   
 
-if [  "$FILE_PATH" ]; then
-   echo "you have permissions to edit $FILE_PATH"
-else 
-   echo "you do not have permissions to execute $FILE_PATH"
-fi
+# if [  "$FILE_PATH" ]; then
+#    echo "you have permissions to edit $FILE_PATH"
+# else 
+#    echo "you do not have permissions to execute $FILE_PATH"
+# fi
+
+animals="pig,cow,lion,elephant"
+for i in $animals
+do 
+  echo $i
+done
