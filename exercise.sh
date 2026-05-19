ae51d0f5318c4cf54dc38bd3823af917af1dad57
Poguri Diya <diya>
2026-05-19 16:59:05 +0530

    shell

--- exercise.sh
+++ exercise.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-VARIABLE="shell scripting is fun!"
-echo $VARIABLE
+# VARIABLE="shell scripting is fun!"
+# echo $VARIABLE
+
+HOSTNAME="hostname"
+echo  "this script is running on $HOSTNAME"
